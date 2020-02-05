59ccb42be0dbd933f497414ac5ee7932dee1b5f8
rahul799 <rahulagrawal799110@gmail.com>
2020-02-05 22:03:18 +0530

    running-prettier

--- base/logger/docs/docsearch.js
+++ base/logger/docs/docsearch.js
@@ -1,8 +1,7 @@
 $(function() {
-
   // register a handler to move the focus to the search bar
   // upon pressing shift + "/" (i.e. "?")
-  $(document).on('keydown', function(e) {
+  $(document).on("keydown", function(e) {
     if (e.shiftKey && e.keyCode == 191) {
       e.preventDefault();
       $("#search-input").focus();
@@ -13,18 +12,17 @@ $(function() {
     // do keyword highlighting
     /* modified from https://jsfiddle.net/julmot/bL6bb5oo/ */
     var mark = function() {
-
-      var referrer = document.URL ;
-      var paramKey = "q" ;
+      var referrer = document.URL;
+      var paramKey = "q";
 
       if (referrer.indexOf("?") !== -1) {
-        var qs = referrer.substr(referrer.indexOf('?') + 1);
-        var qs_noanchor = qs.split('#')[0];
-        var qsa = qs_noanchor.split('&');
+        var qs = referrer.substr(referrer.indexOf("?") + 1);
+        var qs_noanchor = qs.split("#")[0];
+        var qsa = qs_noanchor.split("&");
         var keyword = "";
 
         for (var i = 0; i < qsa.length; i++) {
-          var currentParam = qsa[i].split('=');
+          var currentParam = qsa[i].split("=");
 
           if (currentParam.length !== 2) {
             continue;
@@ -71,14 +69,18 @@ function matchedWords(hit) {
 }
 
 function updateHitURL(hit) {
-
   var words = matchedWords(hit);
   var url = "";
 
   if (hit.anchor) {
-    url = hit.url_without_anchor + '?q=' + escape(words.join(" ")) + '#' + hit.anchor;
+    url =
+      hit.url_without_anchor +
+      "?q=" +
+      escape(words.join(" ")) +
+      "#" +
+      hit.anchor;
   } else {
-    url = hit.url + '?q=' + escape(words.join(" "));
+    url = hit.url + "?q=" + escape(words.join(" "));
   }
 
   return url;
