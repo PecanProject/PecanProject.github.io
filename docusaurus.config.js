b7625180d0f4dadc69ca3ed47cb24fe95ca2fb51
Gandalf <whogandalf@gmail.com>
2024-04-29 17:56:32 +0530

    set base path to https://pecanproject.github.io

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,7 +4,7 @@ module.exports = {
   tagline:
     "Ecosystem science, policy, and management informed by the best available data and models",
   url: "https://pecanproject.github.io",
-  baseUrl: "/web/",
+  baseUrl: "/",
   onBrokenLinks: "ignore",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
