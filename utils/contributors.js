e0f336f556fe82c0eabcbe3be89f47dc67388662
dlebauer <dlebauer@gmail.com>
2024-05-10 21:37:24 +0000

    Deploying to gh-pages from @ PecanProject/PecanProject.github.io@d2d93d35eee1dbed7c727d9032b6cb18f81abd08 🚀

--- utils/contributors.js
+++ utils/contributors.js
@@ -8,9 +8,9 @@ const contributors = [
   },
   {
     name: "Dr. David LeBauer",
-    email: "dlebauer@email.arizona.edu",
+    email: "dlebauer@gmail.com",
     role: "PEcAn Co-PI",
-    profession: "Staff Scientist, Indigo Ag",
+    profession: "Scientist, The LeBauer Approach LLC and University of Arizona",
     img: "https://datascience.cct.arizona.edu/sites/datascience.cct.arizona.edu/files/styles/az_medium/public/2023-11/david_lebauer.jpeg",
   },
   {
@@ -36,16 +36,16 @@ const contributors = [
   },
   {
     name: "Shawn Serbin",
-    email: "sserbin@bnl.gov",
+    email: "shawn.p.serbin@nasa.gov",
     role: "",
-    profession: "Associate Scientist, Brookhaven National Laboratory",
+    profession: "Supervisory Research AST Earth Science Remote Sensing, NASA Goddard Space Flight Center",
     img: "https://ngee-tropics.lbl.gov/wp-content/uploads/sites/16/2017/01/Shawn-Serbin.jpg",
   },
   {
     name: "Christopher Black",
     email: "chris@ckblack.org",
     role: "",
-    profession: "Senior Scientist, Indigo Ag",
+    profession: "Scientist, Pools and Fluxes LLC",
     img: "https://ckblack.org/2014/04/ckb-20140427-300px.jpg",
   },
   {
@@ -54,13 +54,6 @@ const contributors = [
     role: "",
     profession: "Senior researcher, Finnish Meteorological Institute",
     img: "https://people.bu.edu/dietze/images/ifp_photo-426x636.jpg",
-  },
-  {
-    name: "Hamze Dokoohaki",
-    email: "hamzed@illinois.edu",
-    role: "",
-    profession: "Staff Scientist, Indigo Ag",
-    img: "https://scholar.googleusercontent.com/citations?view_op=view_photo&user=91lPlxQAAAAJ&citpid=7",
   },
   {
     name: "Alexey Shiklomanov",
@@ -73,15 +66,8 @@ const contributors = [
     name: "Dr. Betsy Cowdery",
     email: "",
     role: "",
-    profession: "Graduate Student, Boston University",
+    profession: "Postdoctoral Researcher, The James Hutton Institute",
     img: "https://people.bu.edu/dietze/images/04-elizabeth.jpg",
-  },
-  {
-    name: "Tony Gardella",
-    email: "",
-    role: "",
-    profession: "Project Manager, Boston University",
-    img: "https://pecanproject.github.io/images/tonyg_pic.jpg",
   },
   {
     name: "Dongchen Zhang",
@@ -96,6 +82,27 @@ const contributors = [
     role: "Research Scientist",
     profession: "Brookhaven National Lab",
     img: "https://www.bnl.gov/assets/global/images/render.php?q=0|26462.jpg|500", 
+  },
+  {
+    name: "Meet Agarwal",
+    email: "agrawalmeet91@gmail.com",
+    role: "GSoC'23 Contributor",
+    profession: "Software Engineer",
+    img: "https://64.media.tumblr.com/c8143e590d0312d730d5dd42e345126a/e1e32936de328bec-90/s1280x1920/8e05bb187c1458f41f0c341ddb8f91d5e228ed77.jpg",
+  },
+  {
+    name: "Nihar Sanda",
+    email: "nihar.sanda@gmail.com",
+    role: "GSoC'22 & GSoC'23 Contributor; GSoC '24 Mentor",
+    profession: "Research Associate, IIT Bombay",
+    img: "https://koolgax99.github.io/nihar.jpg",
+  },
+  {
+    name: "Rohan Sasne",
+    email: "rohansasne30@gmail.com",
+    role: "GSoC'23 Contributor",
+    profession: "Co-Founder and CEO, Dynalize Technologies",
+    img: "https://avatars.githubusercontent.com/u/110545952?v=4",
   },
 ];
 export default contributors;
