7ad8119d06265526610cbde06f2702310e722d90
Chris Black <chris@ckblack.org>
2025-05-27 00:12:29 -0700

    fix link to package documentation

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,7 +26,7 @@ module.exports = {
               to: "/documentation", label: "Documentation",
             },
             {
-              to: "/documentation/master", label: "Package Documentation",
+              to: "/package-documentation/develop", label: "Package Documentation",
             },
             {
               to: "/tutorials", label: "Tutorials",
